ec50d390ecd997fb4800ff54701c3d8bb5131f35
Arlen Peiffer <arlenpeiffer@gmail.com>
2026-07-06 15:09:09 -0700

    setup linter and formatter

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -1,7 +1,8 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemaTypes'
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+
+import { schemaTypes } from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
@@ -13,6 +14,6 @@ export default defineConfig({
   plugins: [structureTool(), visionTool()],
 
   schema: {
-    types: schemaTypes,
-  },
+    types: schemaTypes
+  }
 })
